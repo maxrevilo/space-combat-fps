0829d5ea924993ff3bc56d0035bcb8c006cd0089
Oliver Perez <oliver.a.perez.c@gmail.com>
2016-01-16 19:47:03 -0500

    Fixes gyro control

--- Assets/Game/scripts/ShipControl.cs
+++ Assets/Game/scripts/ShipControl.cs
@@ -2,73 +2,40 @@
 using System.Collections;
 using System;
 
+[RequireComponent(typeof(ShipMotor))]
 public class ShipControl : MonoBehaviour {
 
-	private Gyroscope gyro;
+    [SerializeField]
+    private bool useClassicControls = false;
 
-	Vector3 calibratedPos;
-	int calibrationSamplesLeft = 10;
-	int takenCalibrationSamples = 0;
+    private TiltJoystick tiltJoystick;
 
-	[SerializeField]
-	Vector3 gyroCtrlRange = new Vector3(1,1,1);
-	[SerializeField]
-	Vector3 gyroLPFilter = new Vector3(1,1,1);
+    private ShipMotor shipMotor;
 
-	Vector3 gyroSmothPosition = new Vector3(0, 0, 0);
+    void Awake()
+    {
+        shipMotor = GetComponent<ShipMotor>();
 
-	// Use this for initialization
-	void Start () {
-		if (SystemInfo.supportsGyroscope) {
-			gyro = Input.gyro;
-			gyro.enabled = true;
-		}
-	}
+        tiltJoystick = GetComponent<TiltJoystick>();
+    }
 
-	void FixedUpdate() {
-		if (calibrationSamplesLeft > 0) {
-			calibrationSamplesLeft--;
-			calibratedPos += gyro.attitude.eulerAngles;
-			takenCalibrationSamples++;
+    void FixedUpdate()
+    {
+        float lateral, vertical;
 
-			if (calibrationSamplesLeft == 0) {
-				calibratedPos = calibratedPos / takenCalibrationSamples;
-				Debug.Log ("Calibrated on " + calibratedPos);
-			}
-		} else {
-			updateControls ();
+        shipMotor.setThrust(0.5f);
 
-		}
-	}
+        if(useClassicControls || tiltJoystick == null || !tiltJoystick.enabled)
+        {
+            vertical = Input.GetAxis("Vertical");
+            lateral = Input.GetAxis("Horizontal");
+        } else
+        {
+            vertical = tiltJoystick.getPitch();
+            lateral = tiltJoystick.getRoll();
+        }
 
-	// Update is called once per frame
-	void Update () {
-		if (calibrationSamplesLeft == 0) {
-			Debug.Log (String.Format ("{0}   <<<<<  {1}", gyroSmothPosition, gyro.attitude.eulerAngles));
-
-			Vector3 dir = new Vector3 (getPitch (), getYaw(), 0);
-
-			transform.rotation = Quaternion.Euler (dir);
-		}
-	}
-
-	void updateControls() {
-		Vector3 sample = gyro.attitude.eulerAngles - calibratedPos;
-		Vector3 diff = sample - gyroSmothPosition;
-		gyroSmothPosition.x += (diff.x) * gyroLPFilter.x;
-		gyroSmothPosition.y += (diff.y) * gyroLPFilter.y;
-		gyroSmothPosition.z += (diff.z) * gyroLPFilter.z;
-
-		gyroSmothPosition.x = gyroSmothPosition.x / gyroCtrlRange.x;
-		gyroSmothPosition.y = gyroSmothPosition.y / gyroCtrlRange.y;
-		gyroSmothPosition.z = gyroSmothPosition.z / gyroCtrlRange.z;
-	}
-
-	float getPitch() {
-		return gyroSmothPosition.y;
-	}
-
-	float getYaw() {
-		return gyroSmothPosition.z;
-	}
+        shipMotor.setPitchAngle(vertical);
+        shipMotor.setTurnAmount(lateral);
+    }
 }
